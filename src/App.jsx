98a01d2206f9940fb39a31d54de1c32906447d39
Jagadeeshftw <jagadeesh26062002@gmail.com>
2024-05-24 00:56:44 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import SideBar from "../Components/SideBar"
+import Theme from "../Components/Theme"
+
+function App() {
+
+  return (
+    <>
+      <Theme/>
+      <SideBar/>
+    </>
+  )
+}
+
+export default App
